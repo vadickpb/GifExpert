05d7e94d67b950b47ced9eecbe2c18ae34b6057c
vadickpb <vadick1@gmail.com>
2020-10-20 21:46:47 -0500

    agregando docs

---
+++ docs/precache-manifest.7b5645e9dd0eb3c24a3e7524258c8872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e62502ae2c62e7f7a536471c66ec22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f64baf10b21839be3e11",
+    "url": "/static/css/main.2598f55e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64baf10b21839be3e11",
+    "url": "/static/js/main.fa3f974d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
